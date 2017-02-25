06d3fc51862792c92fb152ad64ecb89a44f8fa5f
Richard Bramley <rabramley@gmail.com>
2017-02-25 14:51:12 +0000

    Application, of course

--- downloads/downloads/wsgi_live.py
+++ downloads/downloads/wsgi_live.py
@@ -2,7 +2,7 @@ from downloads import create_app
 from downloads.database import upgrade_db
 from config import LiveConfig
 
-app = create_app(LiveConfig)
+application = create_app(LiveConfig)
 
-with app.app_context():
-    upgrade_db(app)
+with application.app_context():
+    upgrade_db(application)
