3ffbf6397affa9b65270408f9f5b0e976fe36a71
Richard Bramley <rabramley@gmail.com>
2017-02-25 15:28:52 +0000

    Because it's impossible to debug

--- downloads/config.py
+++ downloads/config.py
@@ -1,22 +1,18 @@
 import os
-import logging
 
 
 class LiveConfig(object):
     """configuration options for live
         environment variables are set in Apache http.conf
     """
-    SECRET_KEY = os.getenv('FLASK_SECRET_KEY', '')
+    SECRET_KEY = os.getenv('FLASK_SECRET_KEY', 'oihoifhoqhf[r fpihqeriufh')
     DEBUG = False
-    DB_NAME = os.getenv('DB_NAME', '')
-    DB_USER = os.getenv('DB_USER', '')
-    DB_PASS = os.getenv('DB_PASSWORD', '')
-    SQLALCHEMY_DATABASE_URI = 'mysql://{0}:{1}@localhost/{2}'.format(
+    DB_NAME = os.getenv('DB_NAME', 'downloads')
+    DB_USER = os.getenv('DB_USER', 'lampuser')
+    DB_PASS = os.getenv('DB_PASSWORD', 'PeRin1')
+    SQLALCHEMY_DATABASE_URI = 'mysql://{0}:{1}@127.0.0.1/{2}'.format(
         DB_USER, DB_PASS, DB_NAME
     )
-    logging.error('HHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH')
-    print(SQLALCHEMY_DATABASE_URI)
-    SQLALCHEMY_DATABASE_URI = 'mysql://lampuser:PeRin1@localhost/downloads'
     BASE_DIR = os.path.abspath(os.path.dirname(__file__))
     DOWNLOAD_DIR = os.path.join(BASE_DIR, 'files')
     WTF_CSRF_ENABLED = True
