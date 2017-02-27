7cf4162a924fa793df9d9937dc5951b12777790e
Richard Bramley <rabramley@gmail.com>
2017-02-27 09:48:53 +0000

    Import from file

--- downloads/downloads/__init__.py
+++ downloads/downloads/__init__.py
@@ -5,11 +5,15 @@ from config import BaseConfig
 from downloads.forms import DownloadForm
 from downloads.database import initialise_db, db
 from downloads.models import Download
+import logging
 
 
 def create_app(config=BaseConfig):
     app = Flask(__name__)
     app.config.from_object(config)
+    app.config.from_pyfile('application.cfg', silent=True)
+
+    logging.error(app.config['SQLALCHEMY_DATABASE_URI'])
 
     with app.app_context():
         initialise_db(app)
--- downloads/downloads/wsgi_live.py
+++ downloads/downloads/wsgi_live.py
@@ -1,18 +1,7 @@
-import os
-import logging
 from downloads import create_app
 from downloads.database import upgrade_db
-from config import LiveConfig
 
+application = create_app()
 
-def application(environ, start_response):
-    for key in ['FLASK_SECRET_KEY', 'DB_NAME', 'DB_USER', 'DB_PASSWORD']:
-        os.environ[key] = environ.get(key, '')
-        logging.error(os.environ[key])
-
-    app = create_app(LiveConfig())
-
-    with app.app_context():
-        upgrade_db(app)
-
-    return [app]
+with application.app_context():
+    upgrade_db(application)
