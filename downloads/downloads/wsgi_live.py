38ddf86443de3d0b3019eea513b4532b9aca4882
Richard Bramley <rabramley@gmail.com>
2017-02-27 09:18:13 +0000

    Import later

--- downloads/downloads/wsgi_live.py
+++ downloads/downloads/wsgi_live.py
@@ -2,13 +2,14 @@ import os
 import logging
 from downloads import create_app
 from downloads.database import upgrade_db
-from config import LiveConfig
 
 
 def application(environ, start_response):
     for key in ['FLASK_SECRET_KEY', 'DB_NAME', 'DB_USER', 'DB_PASSWORD']:
         os.environ[key] = environ.get(key, '')
         logging.error(os.environ[key])
+
+    from config import LiveConfig
 
     app = create_app(LiveConfig)
 
